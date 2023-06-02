52fd31e41195307f0af5795caf9ddf9edfed4f22
Sahil <sahil@Sahils-MacBook-Pro.local>
2023-06-02 13:03:31 +0530

    error red

--- src/components/Right.js
+++ src/components/Right.js
@@ -5,8 +5,9 @@ const Right = () => {
   const [email, setEmail] = useState("");
   const [passType, setPassType] = useState("password");
   const [showPass, setShowPass] = useState(false);
-  const [password, setPass] = useState("")
-  
+  const [password, setPass] = useState("");
+  const [prompt, setPrompt] = useState(false);
+
   const handleEmailInput = (e) => {
     setEmail(e.target.value);
   };
@@ -18,9 +19,16 @@ const Right = () => {
       setPassType("text");
     }
   };
+  const handleDetails = () => {
+    if (password == null) {
+      setPrompt(false);
+    } else {
+      setPrompt(true);
+    }
+  };
   const handlePass = (e) => {
-    setPass(e.target.value)
-  }
+    setPass(e.target.value);
+  };
   return (
     <>
       <div className="right_component">
@@ -29,41 +37,70 @@ const Right = () => {
         </div>
         <p>Welcome to &lt;product name&gt;. Please Login to your account</p>
         <h3>College Id </h3>
-        <input
-          className="input_style_1"
-          type="text"
-          value={email}
-          onChange={handleEmailInput}
-          placeholder="id@pvppcoe.ac.in"
-        ></input>
-        <h3>Password</h3>
-        <div>
+        {prompt ? (
           <input
-            className="input_style_2"
-            type={passType}
-            onChange={handlePass}
-            value={password}
-            placeholder="password"
+            className="input_style_1_modal"
+            type="text"
+            placeholder="id@pvppcoe.ac.in"
           ></input>
-          <button onClick={handleClick} className="show_pass">
-            <img
-              width="20"
-              height="20"
-              src="https://img.icons8.com/ios-glyphs/20/visible--v1.png"
-              alt="visible--v1"
-            />
-          </button>
-        </div>
-        {(password.length < 8 && password.length != 0)?<div>minlength is 8 chars</div> : null}
+        ) : (
+          <input
+            className="input_style_1"
+            type="text"
+            value={email}
+            onChange={handleEmailInput}
+            placeholder="id@pvppcoe.ac.in"
+          ></input>
+        )}
+        <h3>Password</h3>
+        {prompt ? (
+          <div>
+            <input
+              className="input_style_2_modal"
+              placeholder="password"
+            ></input>
+            <button className="show_pass">
+              <img
+                width="20"
+                height="20"
+                src="https://img.icons8.com/ios-glyphs/20/visible--v1.png"
+                alt="visible--v1"
+              />
+            </button>
+          </div>
+        ) : (
+          <div>
+            <input
+              className="input_style_2"
+              type={passType}
+              onChange={handlePass}
+              value={password}
+              placeholder="password"
+            ></input>
+            <button onClick={handleClick} className="show_pass">
+              <img
+                width="20"
+                height="20"
+                src="https://img.icons8.com/ios-glyphs/20/visible--v1.png"
+                alt="visible--v1"
+              />
+            </button>
+          </div>
+        )}
+        {password.length < 8 && password.length != 0 ? (
+          <div>minlength is 8 chars</div>
+        ) : null}
         <div className="submission_login">
-          <button className="submit" type="submit">
+          <button className="submit" type="submit" onClick={handleDetails}>
             Login
           </button>
-          <Link to="/signup"><p>Create Account</p></Link>
+          <Link to="/signup">
+            <p>Create Account</p>
+          </Link>
         </div>
       </div>
       <div className="web_footer">
-       <p>www.getflytechnologies.com</p>
+        <p>www.getflytechnologies.com</p>
       </div>
     </>
   );
